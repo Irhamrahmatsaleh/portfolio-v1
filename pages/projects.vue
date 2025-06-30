3873f0c8cf72e82cddd7fb7f9a30f8d2152f4463
Irham Rahmat Saleh <irhamrahmatsaleh1997@gmail.com>
2025-06-30 19:14:37 +0700

    fix: project

--- pages/projects.vue
+++ pages/projects.vue
@@ -55,7 +55,7 @@
       <!-- projects -->
       <div id="projects-case" class="grid grid-cols-1 lg:grid-cols-2 max-w-full h-full overflow-scroll lg:self-center">
         <div id="not-found"
-          class="hidden flex flex-col font-fira_retina text-menu-text my-5 h-full justify-center items-center">
+          class="hidden flex-col font-fira_retina text-menu-text my-5 h-full justify-center items-center">
           <span class="flex justify-center text-4xl pb-3">
             X__X
           </span>
@@ -80,7 +80,7 @@ import DevConfig from '~/developer.json';
 
 const config = ref(DevConfig)
 
-const techs = ['React', 'HTML', 'CSS', 'Vue', 'Angular', 'Gatsby', 'Flutter']
+const techs = ['react', 'html', 'css', 'vue', 'angular', 'gatsby', 'flutter']
 const filters = ref(['all'])
 const showFilters = ref(true)
 const projects = ref(config.value.projects)
