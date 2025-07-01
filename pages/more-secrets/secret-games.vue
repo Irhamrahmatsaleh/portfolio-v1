dd96c18e903e7e7f28ad620cfd22d6146d2b5053
Irham Rahmat Saleh <irhamrahmatsaleh1997@gmail.com>
2025-07-01 18:48:34 +0700

    fix: game

--- pages/more-secrets/secret-games.vue
+++ pages/more-secrets/secret-games.vue
@@ -6,9 +6,14 @@
 </template> -->
 
 
-<template>
+<!-- <template>
   <div class="editor-embed">
     <iframe src="https://www.onlinegames.io/t/embeddable-games-for-websites/" width="100%" height="650" frameborder="0"
       style="border-radius:8px;box-shadow:0 4px 24px #0002" allowfullscreen></iframe>
   </div>
+</template> -->
+
+<template>
+  <iframe style="width: 100%; height: 550px; overflow: hidden; border: none;"
+    src="https://playpager.com/embed/cubes/index.html" title="Cubes Game" scrolling="no"></iframe>
 </template>
