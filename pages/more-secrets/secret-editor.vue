863bcc6348c8f7f125d1c000d066f3e595258ae8
Irham Rahmat Saleh <138499395+Irhamrahmatsaleh@users.noreply.github.com>
2025-07-02 07:04:36 +0700

    Update secret-editor.vue

--- pages/more-secrets/secret-editor.vue
+++ pages/more-secrets/secret-editor.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="editor-embed">
-    <iframe src="https://stackblitz.com/edit/stackblitz-starters-dmsrzves?embed=1&file=index.js" width="100%" height="650"
+    <iframe src="https://stackblitz.com/edit/nuxt-starter-koqnfmmz?embed=1&file=README.md" width="100%" height="650"
       frameborder="0" style="border-radius:8px;box-shadow:0 4px 24px #0002" allowfullscreen></iframe>
   </div>
 </template>
